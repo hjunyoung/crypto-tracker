b9f3266ce3d5e3b20012ee25c5314e108ac2b955
Junyoung Hong <hjunyoung10@gmail.com>
2022-01-24 16:15:19 +0900

    🔨 Add a fetchCoinHistory fetcher function

--- src/api.ts
+++ src/api.ts
@@ -15,3 +15,11 @@ export const fetchCoinPrice = (coinId: string) => {
     response.json()
   );
 };
+
+export const fetchCoinHistory = (coinId: string) => {
+  const endDate = Math.floor(Date.now() / 1000);
+  const startDate = endDate - 2 * 7 * 24 * 60 * 60;
+  return fetch(
+    `${BASE_URL}/coins/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`
+  ).then((response) => response.json());
+};
