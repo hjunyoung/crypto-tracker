6a72d00fd7d109add2be916f0083696f19065487
Junyoung Hong <hjunyoung10@gmail.com>
2022-01-24 12:00:44 +0900

    ⚗️ Add a react query dev tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Router from './Router';
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -31,6 +32,7 @@ function App() {
     <>
       <GlobalStyle />
       <Router />
+      <ReactQueryDevtools initialIsOpen={true} />
     </>
   );
 }
